0dd462938f628a512a101660085faf8734508a0b
Joelmir R. Carvalho <joalves3carvalho@gmail.com>
2023-12-18 14:58:07 -0300

    97. Criando um binding

--- lib/src/pages/home/home_tab.dart
+++ lib/src/pages/home/home_tab.dart
@@ -1,11 +1,13 @@
 import 'package:add_to_cart_animation/add_to_cart_animation.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:greengrocer/src/config/custom_colors.dart';
 import 'package:greengrocer/src/pages/common_widgets/app_name_widget.dart';
 import 'package:greengrocer/src/pages/common_widgets/custom_shimmer.dart';
 import 'package:greengrocer/src/pages/home/components/category_tile.dart';
 import 'package:greengrocer/src/config/app_data.dart' as app_data;
 import 'package:greengrocer/src/pages/home/components/item_tile.dart';
+import 'package:greengrocer/src/pages/home/controller/home_controller.dart';
 
 class HomeTab extends StatefulWidget {
   const HomeTab({Key? key}) : super(key: key);
@@ -32,6 +34,8 @@ class _HomeTabState extends State<HomeTab> {
         isLoading = false;
       });
     });
+
+    Get.find<HomeController>().printExample();
   }
 
   @override
