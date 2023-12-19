47d0ed741ec62478dd749b9137cf411170d60a06
Joelmir R. Carvalho <joalves3carvalho@gmail.com>
2023-12-19 11:07:50 -0300

    98. Criando feezed com generics

---
+++ lib/src/pages/home/result/home_result.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'home_result.freezed.dart';
+
+@freezed
+class HomeResult with _$HomeResult {
+  factory HomeResult.success(List data) = Success;
+  factory HomeResult.error(String message) = Error;
+}
