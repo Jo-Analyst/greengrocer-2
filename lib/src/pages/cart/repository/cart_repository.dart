6952a7a65a722980961bc1e403ab2438b0c5db85
Joelmir R. Carvalho <joalves3carvalho@gmail.com>
2023-12-22 09:49:44 -0300

    114. Inicio do desenvolvimento do cart repository

---
+++ lib/src/pages/cart/repository/cart_repository.dart
@@ -0,0 +1,24 @@
+import 'package:greengrocer/src/constants/endpoints.dart';
+import 'package:greengrocer/src/services/http_manager.dart';
+
+class CartRepository {
+  final _httpManager = HttpManager();
+
+  Future getCartItems({
+    required String token,
+    required userId,
+  }) async {
+    final result = await _httpManager.restRequest(
+      url: Endpoints.getCartItem,
+      method: HttpMethods.post,
+      header: {'X-Parse-Session-Token': token},
+      body: {'user': userId},
+    );
+
+    if (result['result'] != null) {
+      print(result['result']);
+    } else {
+      print("Ocorreu um erro");
+    }
+  }
+}
